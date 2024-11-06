fba48b93dacd91994bf9d1b16e3d840e71edd757
Longden3z <162593267+LongPH51747@users.noreply.github.com>
2024-11-06 16:31:00 +0700

    Update MainActivity.java

--- app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
+++ app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
@@ -1,9 +1,4 @@
-<<<<<<<< HEAD:app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
 package fpoly.longlt.duan1.screen;
-========
-package fpoly.longlt.duan1.Screen;
->>>>>>>> Bao:app/src/main/java/fpoly/longlt/duan1/Screen/ManHinhCho.java
-
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -14,12 +9,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import fpoly.longlt.duan1.R;
 
-<<<<<<<< HEAD:app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
 public class MainActivity extends AppCompatActivity {
-========
-public class ManHinhCho extends AppCompatActivity {
->>>>>>>> Bao:app/src/main/java/fpoly/longlt/duan1/Screen/ManHinhCho.java
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
