ca0f83dc6dbf5d6590c69f14ed4fcf2926f4300a
Longden3z <mwglongden7112005@gmail.com>
2024-11-06 15:51:24 +0700

    thêm model

--- app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
+++ app/src/main/java/fpoly/longlt/duan1/screen/MainActivity.java
@@ -1,4 +1,4 @@
-package fpoly.longlt.duan1;
+package fpoly.longlt.duan1.screen;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import fpoly.longlt.duan1.R;
 
 public class MainActivity extends AppCompatActivity {
 
