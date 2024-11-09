e9695c1d76fb18c0db16a2c0fd8029d23184f1ba
Longden3z <mwglongden7112005@gmail.com>
2024-11-09 13:55:17 +0700

    thay đổi csdl

--- app/src/main/java/fpoly/longlt/duan1/database/DBHelper.java
+++ app/src/main/java/fpoly/longlt/duan1/database/DBHelper.java
@@ -19,16 +19,12 @@ public class DBHelper extends SQLiteOpenHelper {
                 "user_id integer primary key autoincrement," +
                 " username text," +
                 " password text," +
-                "role integer default 0)";
-        db.execSQL(createUser);
-        String createKH = "create table khachhang(" +
-                "kh_id integer primary key autoincrement," +
-                "user_id integer references user," +
-                "tenkh text," +
+                "name text," +
                 "sdt text," +
                 "address text," +
-                "moneyonl integer default 0)";
-        db.execSQL(createKH);
+                "moneyonl integer default 0," +
+                "role integer default 0)";
+        db.execSQL(createUser);
         String createSP = "create table sanpham(" +
                 "sp_id integer primary key autoincrement," +
                 "tensp text," +
@@ -38,23 +34,16 @@ public class DBHelper extends SQLiteOpenHelper {
         String createChiTietSP = "create table chitietsp(" +
                 "chitietsp_id integer primary key autoincrement," +
                 "sp_id integer references sanpham," +
-                "name_sp text," +
                 "description text," +
-                "price integer," +
                 "size text," +
                 "color text," +
-                "img text," +
                 "soluong integer)";
         db.execSQL(createChiTietSP);
-        String createCart = "create table cart(" +
-                "cart_id integer primary key autoincrement," +
-                "kh_id integer references khachhang," +
-                "sp_id integer references sanpham," +
-                "quantity integer)";
-        db.execSQL(createCart);
         String createOrder = "create table orders(" +
                 "od_id integer primary key autoincrement," +
-                "kh_id integer references khachhang," +
+                "user_id integer references user," +
+                "vc_id integer references voucher," +
+                "oddetail_id integer references orderdetail," +
                 "od_date text," +
                 "total_price integer," +
                 "status integer)";
@@ -68,6 +57,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL(createOrderDetail);
         String createVoucher = "create table voucher(" +
                 "vc_id integer primary key autoincrement," +
+                "od_id integer references orders," +
                 "code text," +
                 "discount_price integer," +
                 "start_date text," +
@@ -79,10 +69,8 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion != newVersion) {
             db.execSQL("drop table if exists user");
-            db.execSQL("drop table if exists khachhang");
             db.execSQL("drop table if exists sanpham");
             db.execSQL("drop table if exists chitietsp");
-            db.execSQL("drop table if exists cart");
             db.execSQL("drop table if exists orders");
             db.execSQL("drop table if exists orderdetail");
             db.execSQL("drop table if exists voucher");
