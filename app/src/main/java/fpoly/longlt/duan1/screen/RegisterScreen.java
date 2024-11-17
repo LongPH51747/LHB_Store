29f17b228110ab012b6bc80584082994edaebca5
HaVietHai <165687552+HaVietHai@users.noreply.github.com>
2024-11-17 10:44:38 +0700

    Update RegisterScreen.java

--- app/src/main/java/fpoly/longlt/duan1/screen/RegisterScreen.java
+++ app/src/main/java/fpoly/longlt/duan1/screen/RegisterScreen.java
@@ -24,6 +24,7 @@ public class RegisterScreen extends AppCompatActivity {
     private Button btnLogUp;
     private UserDAO userDAO;
     private String phone, name, pass, passConfirm;
+    //hari
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
